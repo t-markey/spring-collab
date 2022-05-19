a769b7932a149a5d4b99dd5adb11fd0ba87b9b6e
ericbolton <eric.bolton@appliedxl.com>
2022-05-19 09:35:37 -0400

    [Add] dummy tests, move files to archive

---
+++ tests/test_margin_factory.py
@@ -0,0 +1,17 @@
+from brownie import MarginFactory
+from scripts.helpful_scripts import get_account
+
+
+def test_can_call_check_upkeep():
+    pass
+    # # Arrange
+    # interval = 2
+    # account = get_account()
+    # counter = Counter.deploy(interval, {"from": account})
+    # upkeepNeeded, performData = counter.checkUpkeep.call(
+    #     "",
+    #     {"from": account},
+    # )
+    # # Assert
+    # assert isinstance(upkeepNeeded, bool)
+    # assert isinstance(performData, bytes)
