79bec36ae0cfbcb04579d1ba7aa6c888f1cac20a
edbltn <edbltn@gmail.com>
2022-05-19 10:29:24 -0400

    Update test_margin_factory.py

--- tests/test_margin_factory.py
+++ tests/test_margin_factory.py
@@ -2,7 +2,7 @@ from brownie import MarginFactory
 from scripts.helpful_scripts import get_account
 
 
-def test_can_call_check_upkeep():
+def test_margin_factory():
     pass
     # # Arrange
     # interval = 2
