b9232127cea51c108087f8e6fe871cbeb6e33d7b
ericbolton <eric.bolton@appliedxl.com>
2022-05-19 09:35:37 -0400

    [Remove] DS_Store, move Github actions

---
+++ tests/test_margin.py
@@ -0,0 +1,14 @@
+from brownie import LeaseMargins
+from scripts.helpful_scripts import get_account, get_contract
+
+
+def test_lease_margins():
+    pass
+    # # Arrange
+    # address = get_contract("eth_usd_price_feed").address
+    # # Act
+    # price_feed = PriceFeedConsumer.deploy(address, {"from": get_account()})
+    # # Assert
+    # value = price_feed.getLatestPrice({"from": get_account()})
+    # assert isinstance(value, int)
+    # assert value > 0
